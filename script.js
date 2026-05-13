245743f1839d902385c2bff86a977923b8aeb66c
Guru Prasad S L <guruprasadgp26207@gmail.com>
2026-05-13 17:43:26 +0530

    template 1 modified

--- script.js
+++ script.js
@@ -444,3 +444,24 @@ el.style.transition =
 
 observer.observe(el);
 });
+
+const reveals =
+document.querySelectorAll(".reveal");
+
+window.addEventListener(
+"scroll",
+()=>{
+
+reveals.forEach(el=>{
+
+const top =
+el.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+
+el.classList.add("active");
+
+}
+
+});
+});
